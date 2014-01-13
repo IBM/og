a228d65b48562b2c92b773b7e03b1f2faebbcfe1
rveitch <rveitch@cleversafe.com>
2014-01-12 20:22:24 -0600

    Added operation components from dsnet-turbine

---
+++ oom-operation/src/main/java/com/cleversafe/oom/operation/OperationType.java
@@ -0,0 +1,41 @@
+//
+// Cleversafe open-source code header - Version 1.3 - January 2, 2009
+//
+// Cleversafe Dispersed Storage(TM) is software for secure, private and
+// reliable storage of the world's data using information dispersal.
+//
+// Copyright (C) 2005-2009 Cleversafe, Inc. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+// USA.
+//
+// Contact Information: Cleversafe, 224 North Desplaines Street, Suite 500
+// Chicago, IL 60661, USA.
+// email licensing@cleversafe.org
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: May 11, 2013
+// ---------------------
+
+package com.cleversafe.oom.operation;
+
+public enum OperationType
+{
+   READ, WRITE, DELETE, LIST, ALL
+}
---
+++ oom-operation/src/main/java/com/cleversafe/oom/operation/OperationTypeMix.java
@@ -0,0 +1,193 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: abaptist
+//
+// Date: Nov 6, 2012
+// ---------------------
+
+package com.cleversafe.oom.operation;
+
+import java.util.Random;
+
+import org.apache.commons.lang3.Validate;
+
+import com.cleversafe.oom.util.WeightedRandomChoice;
+
+/**
+ * An implementation for generating a sequence of <code>OperationType</code> values.
+ */
+public class OperationTypeMix
+{
+   private final long readPercentage;
+   private final long writePercentage;
+   private final long deletePercentage;
+   private final long floor;
+   private final long midpoint;
+   private final long ceiling;
+   private boolean onlyWrite;
+   private boolean onlyDelete;
+   private final WeightedRandomChoice<OperationType> operationMix;
+
+   /**
+    * Constructs an <code>OperationTypeMix</code> instance for generating <code>OperationType</code>
+    * values according to the configured IO mix
+    * 
+    * @param readPercentage
+    * @param writePercentage
+    * @param deletePercentage
+    * @param floor
+    *           the lowest vault fill for deletes to take place, in bytes
+    * @param ceiling
+    *           the highest vault fill for writes to take places, in bytes
+    * @throws IllegalArgumentException
+    *            if readPercentage is negative
+    * @throws IllegalArgumentException
+    *            if writePercentage is negative
+    * @throws IllegalArgumentException
+    *            if deletePercentage is negative
+    * @throws IllegalArgumentException
+    *            if floor is negative
+    * @throws IllegalArgumentException
+    *            if ceiling is less than floor
+    */
+   public OperationTypeMix(
+         final long readPercentage,
+         final long writePercentage,
+         final long deletePercentage,
+         final long floor,
+         final long ceiling)
+   {
+      this(readPercentage, writePercentage, deletePercentage, floor, ceiling, new Random());
+   }
+
+   /**
+    * Constructs an <code>OperationTypeMix</code> instance for generating <code>OperationType</code>
+    * values according to the configured IO mix and using the provided <code>Random</code> instance
+    * for random seed data
+    * 
+    * @param readPercentage
+    * @param writePercentage
+    * @param deletePercentage
+    * @param floor
+    *           the lowest vault fill for deletes to take place, in bytes
+    * @param ceiling
+    *           the highest vault fill for writes to take places, in bytes
+    * @throws IllegalArgumentException
+    *            if readPercentage is negative
+    * @throws IllegalArgumentException
+    *            if writePercentage is negative
+    * @throws IllegalArgumentException
+    *            if deletePercentage is negative
+    * @throws IllegalArgumentException
+    *            if floor is negative
+    * @throws IllegalArgumentException
+    *            if ceiling is less than floor
+    * @throws NullPointerException
+    *            if random is null
+    */
+   public OperationTypeMix(
+         final long readPercentage,
+         final long writePercentage,
+         final long deletePercentage,
+         final long floor,
+         final long ceiling,
+         final Random random)
+   {
+      Validate.isTrue(0 <= readPercentage && readPercentage <= 100,
+            "readPercentage must be in range [0, 100] [%s]", readPercentage);
+      Validate.isTrue(0 <= writePercentage && writePercentage <= 100,
+            "writePercentage must be in range [0, 100] [%s]", writePercentage);
+      Validate.isTrue(0 <= deletePercentage && deletePercentage <= 100,
+            "deletePercentage must be in range [0, 100] [%s]", deletePercentage);
+      final long sumPercentage = readPercentage + writePercentage + deletePercentage;
+      Validate.isTrue(sumPercentage == 100, "Sum of percentages must be 100 [%s]", sumPercentage);
+      Validate.isTrue(floor >= 0, "floor must be >= 0 [%s]", floor);
+      Validate.isTrue(ceiling >= floor, "ceiling must be >= floor [%s]", ceiling);
+      Validate.notNull(random, "random must not be null");
+
+      this.readPercentage = readPercentage;
+      this.writePercentage = writePercentage;
+      this.deletePercentage = deletePercentage;
+      this.operationMix = new WeightedRandomChoice<OperationType>(random);
+      if (readPercentage > 0)
+         this.operationMix.addChoice(OperationType.READ, readPercentage);
+      if (writePercentage > 0)
+         this.operationMix.addChoice(OperationType.WRITE, writePercentage);
+      if (deletePercentage > 0)
+         this.operationMix.addChoice(OperationType.DELETE, deletePercentage);
+      this.floor = floor;
+      this.midpoint = (floor + ceiling) / 2;
+      this.ceiling = ceiling;
+      this.onlyWrite = false;
+      this.onlyDelete = false;
+   }
+
+   /**
+    * @return the configured read percentage
+    */
+   public long getReadPercentage()
+   {
+      return this.readPercentage;
+   }
+
+   /**
+    * @return the configured write percentage
+    */
+   public long getWritePercentage()
+   {
+      return this.writePercentage;
+   }
+
+   /**
+    * @return the configured delete percentage
+    */
+   public long getDeletePercentage()
+   {
+      return this.deletePercentage;
+   }
+
+   /**
+    * Calculates the next <code>OperationType</code> to use. If the current vault fill is within
+    * floor and ceiling, the configured IO mix is used. Otherwise, writes or deletes will be used to
+    * bring the vault fill to half way between floor and ceiling
+    * 
+    * @param vaultFill
+    *           the current vault fill, in bytes
+    * @return the next <code>OperationType</code> to use
+    */
+   public OperationType getNextOperationType(final long vaultFill)
+   {
+      Validate.isTrue(vaultFill >= 0, "vaultFill must be >= 0 [%s]", vaultFill);
+
+      if (vaultFill < this.floor || (this.onlyWrite && vaultFill < this.midpoint))
+      {
+         this.onlyWrite = true;
+         this.onlyDelete = false;
+         return OperationType.WRITE;
+      }
+      else if (vaultFill > this.ceiling || (this.onlyDelete && vaultFill > this.midpoint))
+      {
+         this.onlyDelete = true;
+         this.onlyWrite = false;
+         return OperationType.DELETE;
+      }
+      else
+      {
+         this.onlyWrite = false;
+         this.onlyDelete = false;
+         return this.operationMix.nextChoice();
+      }
+   }
+}
