552e5aff98c9fe27e60e4c086d0eeae61c95946f
rveitch <rveitch@cleversafe.com>
2014-02-06 15:06:58 -0600

    Added Scheduler and OperationManager interfaces

---
+++ oom-api/src/main/java/com/cleversafe/oom/operation/manager/OperationManager.java
@@ -0,0 +1,29 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Feb 6, 2014
+// ---------------------
+
+package com.cleversafe.oom.operation.manager;
+
+import com.cleversafe.oom.operation.Operation;
+
+public interface OperationManager<T extends Operation>
+{
+   T next();
+
+   void complete(T operation);
+}
