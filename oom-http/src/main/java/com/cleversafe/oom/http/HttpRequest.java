3449025804bf8f2d0970d780bef645111ac2940f
rveitch <rveitch@cleversafe.com>
2014-03-11 16:40:20 -0500

    Moved RequestImpl and ResponseImpl to HttpRequest and HttpResponse respectively, in oom-http

--- oom-http/src/main/java/com/cleversafe/oom/http/HttpRequest.java
+++ oom-http/src/main/java/com/cleversafe/oom/http/HttpRequest.java
@@ -17,46 +17,71 @@
 // Date: Mar 11, 2014
 // ---------------------
 
-package com.cleversafe.oom.operation;
+package com.cleversafe.oom.http;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.net.URL;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.SortedMap;
 
-public class ResponseImpl implements Response
+import com.cleversafe.oom.operation.Entity;
+import com.cleversafe.oom.operation.Method;
+import com.cleversafe.oom.operation.Request;
+
+public class HttpRequest implements Request
 {
-   private final long requestId;
-   private final int statusCode;
+   private final long id;
+   private final String customRequestKey;
+   private final Method method;
+   private final URL url;
    private final SortedMap<String, String> headers;
+   private final Entity entity;
    private final SortedMap<String, String> metadata;
 
-   public ResponseImpl(
-         final long requestId,
-         final int statusCode,
+   public HttpRequest(
+         final long id,
+         final String customRequestKey,
+         final Method method,
+         final URL url,
          final SortedMap<String, String> headers,
+         final Entity entity,
          final SortedMap<String, String> metadata)
    {
-      checkArgument(requestId >= 0, "requestId must be >= 0 [%s]", requestId);
-      this.requestId = requestId;
-      checkArgument(statusCode >= 0, "statusCode must be >= 0 [%s]", statusCode);
-      this.statusCode = statusCode;
+      checkArgument(id >= 0, "id must be >= 0 [%s]", id);
+      this.id = id;
+      this.customRequestKey = customRequestKey;
+      this.method = checkNotNull(method, "method must not be null");
+      this.url = checkNotNull(url, "url must not be null");
       this.headers = checkNotNull(headers, "headers must not be null");
+      this.entity = checkNotNull(entity, "entity must not be null");
       this.metadata = checkNotNull(metadata, "metadata must not be null");
    }
 
    @Override
-   public long getRequestId()
+   public long getId()
    {
-      return this.requestId;
+      return this.id;
    }
 
    @Override
-   public int getStatusCode()
+   public String getCustomRequestKey()
    {
-      return this.statusCode;
+      return this.customRequestKey;
+   }
+
+   @Override
+   public Method getMethod()
+   {
+      return this.method;
+   }
+
+   @Override
+   public URL getURL()
+   {
+      return this.url;
    }
 
    @Override
@@ -69,6 +94,12 @@ public class ResponseImpl implements Response
    public Iterator<Entry<String, String>> headers()
    {
       return this.headers.entrySet().iterator();
+   }
+
+   @Override
+   public Entity getEntity()
+   {
+      return this.entity;
    }
 
    @Override
--- oom-http/src/main/java/com/cleversafe/oom/http/HttpResponse.java
+++ oom-http/src/main/java/com/cleversafe/oom/http/HttpResponse.java
@@ -17,67 +17,48 @@
 // Date: Mar 11, 2014
 // ---------------------
 
-package com.cleversafe.oom.operation;
+package com.cleversafe.oom.http;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.net.URL;
 import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.SortedMap;
 
-public class RequestImpl implements Request
+import com.cleversafe.oom.operation.Response;
+
+public class HttpResponse implements Response
 {
-   private final long id;
-   private final String customRequestKey;
-   private final Method method;
-   private final URL url;
+   private final long requestId;
+   private final int statusCode;
    private final SortedMap<String, String> headers;
-   private final Entity entity;
    private final SortedMap<String, String> metadata;
 
-   public RequestImpl(
-         final long id,
-         final String customRequestKey,
-         final Method method,
-         final URL url,
+   public HttpResponse(
+         final long requestId,
+         final int statusCode,
          final SortedMap<String, String> headers,
-         final Entity entity,
          final SortedMap<String, String> metadata)
    {
-      checkArgument(id >= 0, "id must be >= 0 [%s]", id);
-      this.id = id;
-      this.customRequestKey = customRequestKey;
-      this.method = checkNotNull(method, "method must not be null");
-      this.url = checkNotNull(url, "url must not be null");
+      checkArgument(requestId >= 0, "requestId must be >= 0 [%s]", requestId);
+      this.requestId = requestId;
+      checkArgument(statusCode >= 0, "statusCode must be >= 0 [%s]", statusCode);
+      this.statusCode = statusCode;
       this.headers = checkNotNull(headers, "headers must not be null");
-      this.entity = checkNotNull(entity, "entity must not be null");
       this.metadata = checkNotNull(metadata, "metadata must not be null");
    }
 
    @Override
-   public long getId()
+   public long getRequestId()
    {
-      return this.id;
+      return this.requestId;
    }
 
    @Override
-   public String getCustomRequestKey()
+   public int getStatusCode()
    {
-      return this.customRequestKey;
-   }
-
-   @Override
-   public Method getMethod()
-   {
-      return this.method;
-   }
-
-   @Override
-   public URL getURL()
-   {
-      return this.url;
+      return this.statusCode;
    }
 
    @Override
@@ -90,12 +71,6 @@ public class RequestImpl implements Request
    public Iterator<Entry<String, String>> headers()
    {
       return this.headers.entrySet().iterator();
-   }
-
-   @Override
-   public Entity getEntity()
-   {
-      return this.entity;
    }
 
    @Override
