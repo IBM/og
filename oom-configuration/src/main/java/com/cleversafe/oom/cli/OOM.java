0ba15cd30a43489c6d1a768a045f1e2a489d7480
rveitch <rveitch@cleversafe.com>
2014-03-21 09:37:32 -0500

    Removed jsap and dsnet-platform dependencies

--- oom-configuration/src/main/java/com/cleversafe/oom/cli/OOM.java
+++ oom-configuration/src/main/java/com/cleversafe/oom/cli/OOM.java
@@ -19,20 +19,9 @@
 
 package com.cleversafe.oom.cli;
 
-import java.io.IOException;
-import java.net.URL;
-
-import com.martiansoftware.jsap.JSAP;
-import com.martiansoftware.jsap.JSAPException;
 
 public class OOM
 {
-   private static final String JSAP_RESOURCE_NAME = "oom.jsap";
-
-   public static void main(final String[] args) throws IOException, JSAPException
-   {
-      final URL jsapURL = ClassLoader.getSystemResource(JSAP_RESOURCE_NAME);
-      final JSAP jsap = new JSAP(jsapURL);
-      System.out.println(jsap.getHelp());
-   }
+   public static void main(final String[] args)
+   {}
 }
