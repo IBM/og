383cda9e6e28e59814e97d48cb79d139d78b1fa8
rveitch <rveitch@cleversafe.com>
2014-03-26 12:24:23 -0500

    Added Scheme enum

---
+++ oom-http/src/main/java/com/cleversafe/oom/http/Scheme.java
@@ -0,0 +1,26 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Mar 26, 2014
+// ---------------------
+
+package com.cleversafe.oom.http;
+
+public enum Scheme
+{
+   HTTP,
+   HTTPS
+}
