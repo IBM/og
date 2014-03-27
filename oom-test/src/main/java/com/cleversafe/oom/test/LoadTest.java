4d13d0cbb63cd09dc733bf747f27a4ec0edce5fe
rveitch <rveitch@cleversafe.com>
2014-03-27 14:18:04 -0500

    Reworked LoadTest; scheduling and stopping conditions are now the responsibility of OperationManager

--- oom-test/src/main/java/com/cleversafe/oom/test/LoadTest.java
+++ oom-test/src/main/java/com/cleversafe/oom/test/LoadTest.java
@@ -21,82 +21,50 @@ package com.cleversafe.oom.test;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.util.List;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.cleversafe.oom.api.OperationManager;
 import com.cleversafe.oom.api.OperationManagerException;
 import com.cleversafe.oom.client.Client;
 import com.cleversafe.oom.operation.Request;
 import com.cleversafe.oom.operation.Response;
-import com.cleversafe.oom.scheduling.Scheduler;
-import com.cleversafe.oom.test.condition.StoppingCondition;
 import com.google.common.util.concurrent.ListenableFuture;
 
 public class LoadTest
 {
+   private static Logger _logger = LoggerFactory.getLogger(LoadTest.class);
    private final OperationManager operationManager;
-   private final Scheduler scheduler;
    private final Client client;
-   private final List<StoppingCondition> stoppingConditions;
-   private final boolean shouldSchedule;
    private final ExecutorService executorService;
 
    public LoadTest(
          final OperationManager operationManager,
-         final Scheduler scheduler,
          final Client client,
-         final List<StoppingCondition> stoppingConditions)
+         final ExecutorService executorService)
    {
       this.operationManager = checkNotNull(operationManager, "operationManager must not be null");
-      this.scheduler = checkNotNull(scheduler, "scheduler must not be null");
       this.client = checkNotNull(client, "client must not be null");
-      this.stoppingConditions =
-            checkNotNull(stoppingConditions, "stoppingConditions must not be null");
-      this.shouldSchedule = true;
-      this.executorService = Executors.newCachedThreadPool();
+      this.executorService = checkNotNull(executorService, "executorService must not be null");
    }
 
-   // TODO runTest should not throw an exception
-   public void runTest() throws OperationManagerException
+   public void runTest()
    {
-      while (shouldSchedule())
+      try
       {
-         final Request nextRequest = this.operationManager.next();
-         final ListenableFuture<Response> future = this.client.execute(nextRequest);
-         future.addListener(getListener(future), this.executorService);
-
-         this.scheduler.waitForNext();
-      }
-      // clean up test
-      this.executorService.shutdownNow();
-   }
-
-   private boolean shouldSchedule()
-   {
-      // once triggered, should always return false
-      if (!this.shouldSchedule)
-         return false;
-
-      for (final StoppingCondition condition : this.stoppingConditions)
-      {
-         if (condition.triggered())
-            return false;
-      }
-      return true;
-   }
-
-   public void stopTest()
-   {
-      this.stoppingConditions.add(new StoppingCondition()
-      {
-         @Override
-         public boolean triggered()
+         while (true)
          {
-            return true;
+            final Request nextRequest = this.operationManager.next();
+            final ListenableFuture<Response> future = this.client.execute(nextRequest);
+            future.addListener(getListener(future), this.executorService);
          }
-      });
+      }
+      catch (final OperationManagerException e)
+      {
+         _logger.error("", e);
+      }
    }
 
    private Runnable getListener(final ListenableFuture<Response> future)
@@ -119,11 +87,11 @@ public class LoadTest
          try
          {
             LoadTest.this.operationManager.complete(this.future.get());
-            LoadTest.this.scheduler.complete(this.future.get());
          }
          catch (final Exception e)
          {
             // TODO fix this
+            _logger.error("", e);
          }
       }
    }
