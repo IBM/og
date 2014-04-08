11b86e8e75fc464ea17f763643ae7c4d5100f8ee
rveitch <rveitch@cleversafe.com>
2014-04-08 09:05:41 -0500

    Added SimpleOperationManager

---
+++ oom-http/src/main/java/com/cleversafe/oom/http/operation/manager/SimpleOperationManager.java
@@ -0,0 +1,92 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Apr 7, 2014
+// ---------------------
+
+package com.cleversafe.oom.http.operation.manager;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.List;
+import java.util.Map;
+
+import com.cleversafe.oom.api.Consumer;
+import com.cleversafe.oom.api.OperationManager;
+import com.cleversafe.oom.api.OperationManagerException;
+import com.cleversafe.oom.api.Producer;
+import com.cleversafe.oom.http.HttpRequestContext;
+import com.cleversafe.oom.operation.OperationType;
+import com.cleversafe.oom.operation.OperationTypeMix;
+import com.cleversafe.oom.operation.Request;
+import com.cleversafe.oom.operation.RequestContext;
+import com.cleversafe.oom.operation.Response;
+import com.cleversafe.oom.scheduling.Scheduler;
+
+public class SimpleOperationManager implements OperationManager
+{
+   private final OperationTypeMix operationTypeMix;
+   private final Map<OperationType, Producer<Request>> producers;
+   private final List<Consumer<Response>> consumers;
+   private final Scheduler scheduler;
+   private final Map<Long, Request> pendingRequests;
+
+   public SimpleOperationManager(
+         final OperationTypeMix mix,
+         final Map<OperationType, Producer<Request>> producers,
+         final List<Consumer<Response>> consumers,
+         final Scheduler scheduler,
+         final Map<Long, Request> pendingRequests)
+   {
+      this.operationTypeMix = checkNotNull(mix, "operationTypeMix must not be null");
+      this.producers = checkNotNull(producers, "producers must not be null");
+      this.consumers = checkNotNull(consumers, "consumers must not be null");
+      this.scheduler = checkNotNull(scheduler, "scheduler must not be null");
+      this.pendingRequests = checkNotNull(pendingRequests, "pendingRequests must not be null");
+   }
+
+   @Override
+   public Request next() throws OperationManagerException
+   {
+      this.scheduler.waitForNext();
+
+      final RequestContext context = new HttpRequestContext();
+      // TODO create stats object and use it to pass an appropriate fill to getNextOperationType
+      final OperationType operationType = this.operationTypeMix.getNextOperationType(1);
+      final Producer<Request> producer = this.producers.get(operationType);
+      try
+      {
+         final Request request = producer.produce(context);
+         this.pendingRequests.put(request.getId(), request);
+         return request;
+      }
+      catch (final Exception e)
+      {
+         throw new OperationManagerException(e);
+      }
+   }
+
+   @Override
+   public void complete(final Response response)
+   {
+      for (final Consumer<Response> consumer : this.consumers)
+      {
+         consumer.consume(response);
+      }
+      this.scheduler.complete(response);
+      this.pendingRequests.remove(response.getRequestId());
+   }
+}
