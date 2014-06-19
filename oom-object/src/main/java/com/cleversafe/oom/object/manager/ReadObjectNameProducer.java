5057962e5cfe856a976d704a50c79fc396cb5960
rveitch <rveitch@cleversafe.com>
2014-06-19 14:48:40 -0500

    Added DeleteObjectNameProducer and ReadObjectNameProducer to replace ObjectNameProducer

---
+++ oom-object/src/main/java/com/cleversafe/oom/object/manager/ReadObjectNameProducer.java
@@ -0,0 +1,41 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Mar 29, 2014
+// ---------------------
+
+package com.cleversafe.oom.object.manager;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.cleversafe.oom.api.Producer;
+import com.cleversafe.oom.operation.RequestContext;
+
+public class ReadObjectNameProducer implements Producer<String>
+{
+   private final ObjectManager objectManager;
+
+   public ReadObjectNameProducer(final ObjectManager objectManager)
+   {
+      this.objectManager = checkNotNull(objectManager, "objectManager must not be null");
+   }
+
+   @Override
+   public String produce(final RequestContext context)
+   {
+      return this.objectManager.acquireNameForRead().toString();
+   }
+}
