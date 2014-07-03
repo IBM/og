3f4ae98dbf0ef790b012e0dd8906dba8919f3702
rveitch <rveitch@cleversafe.com>
2014-07-03 15:29:07 -0500

    Refactored OG to shutdown ObjectManager in all cases

--- og-configuration/src/main/java/com/cleversafe/og/cli/OG.java
+++ og-configuration/src/main/java/com/cleversafe/og/cli/OG.java
@@ -52,6 +52,7 @@ import com.google.gson.LongSerializationPolicy;
 import com.google.gson.TypeAdapter;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.ProvisionException;
 import com.martiansoftware.jsap.JSAPResult;
 
 public class OG extends AbstractCLI
@@ -76,11 +77,12 @@ public class OG extends AbstractCLI
 
       _configJsonLogger.info(gson.toJson(config));
 
+      ObjectManager objectManager = null;
       try
       {
          final Injector injector = createInjector(config);
          final Statistics stats = injector.getInstance(Statistics.class);
-         final ObjectManager objectManager = injector.getInstance(ObjectManager.class);
+         objectManager = injector.getInstance(ObjectManager.class);
          final LoadTest test = injector.getInstance(LoadTest.class);
          Runtime.getRuntime().addShutdownHook(new ShutdownHook(Thread.currentThread(), test));
          _consoleLogger.info("Configured.");
@@ -88,27 +90,24 @@ public class OG extends AbstractCLI
          final long timestampStart = System.currentTimeMillis();
          final boolean success = test.runTest();
          final long timestampFinish = System.currentTimeMillis();
+         shutdownObjectManager(objectManager);
+
          if (success)
             _consoleLogger.info("Test Completed.");
          else
             _consoleLogger.error("Test ended abruptly. Check application log for details");
-         try
-         {
-            objectManager.testComplete();
-         }
-         catch (final ObjectManagerException e)
-         {
-            _consoleLogger.error("Error shutting down object manager", e);
-         }
 
          logSummaryBanner();
          final Summary summary = new Summary(stats, timestampStart, timestampFinish);
          _consoleLogger.info("{}", summary);
          _summaryJsonLogger.info(gson.toJson(summary.getSummaryStats()));
+
       }
-      catch (final RuntimeException e)
+      catch (final ProvisionException e)
       {
-         _consoleLogger.error("Error provisioning dependencies", e);
+         shutdownObjectManager(objectManager);
+         _consoleLogger.error("Error provisioning dependencies. Check application log for details");
+         _logger.error("", e);
          System.exit(CONFIGURATION_ERROR);
       }
    }
@@ -154,6 +153,21 @@ public class OG extends AbstractCLI
                   new BehavioralEnumTypeAdapterFactory<SizeUnit>(SizeUnit.class, size))
             .setPrettyPrinting()
             .create();
+   }
+
+   private static void shutdownObjectManager(final ObjectManager objectManager)
+   {
+      if (objectManager != null)
+      {
+         try
+         {
+            objectManager.testComplete();
+         }
+         catch (final ObjectManagerException e)
+         {
+            _consoleLogger.error("Error shutting down object manager", e);
+         }
+      }
    }
 
    private static class ShutdownHook extends Thread
