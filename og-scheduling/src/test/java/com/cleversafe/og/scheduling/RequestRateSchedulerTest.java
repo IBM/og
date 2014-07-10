bf895d6ee1ac74b8b9a3ed6e27379fb08717b663
rveitch <rveitch@cleversafe.com>
2014-07-10 16:23:08 -0500

    Added a RequestRateScheduler test

--- og-scheduling/src/test/java/com/cleversafe/og/scheduling/RequestRateSchedulerTest.java
+++ og-scheduling/src/test/java/com/cleversafe/og/scheduling/RequestRateSchedulerTest.java
@@ -24,10 +24,10 @@ import static org.mockito.Mockito.when;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.cleversafe.og.scheduling.RequestRateScheduler;
 import com.cleversafe.og.util.distribution.Distribution;
 
 public class RequestRateSchedulerTest
@@ -38,7 +38,7 @@ public class RequestRateSchedulerTest
    public void setBefore()
    {
       this.mockDistribution = mock(Distribution.class);
-      when(this.mockDistribution.nextSample()).thenReturn(1.0);
+      when(this.mockDistribution.nextSample()).thenReturn(10.0);
    }
 
    @Test(expected = NullPointerException.class)
@@ -56,6 +56,15 @@ public class RequestRateSchedulerTest
    @Test
    public void testRequestRateScheduler()
    {
-      new RequestRateScheduler(this.mockDistribution, TimeUnit.MILLISECONDS);
+      final RequestRateScheduler s =
+            new RequestRateScheduler(this.mockDistribution, TimeUnit.SECONDS);
+      for (int i = 0; i < 10; i++)
+      {
+         final long timestampStart = System.nanoTime();
+         s.waitForNext();
+         final long duration = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - timestampStart);
+         // 10 per second -> 1 per 100 millis
+         Assert.assertTrue(duration > 50 && duration < 150);
+      }
    }
 }
