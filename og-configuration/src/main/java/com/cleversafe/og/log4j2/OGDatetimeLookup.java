eeb96209a61ed9011602b85dcc11830923c7fe28
rveitch <rveitch@cleversafe.com>
2014-07-11 16:19:42 -0500

    Removed dependency on system date command

---
+++ og-configuration/src/main/java/com/cleversafe/og/log4j2/OGDatetimeLookup.java
@@ -0,0 +1,49 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Jul 11, 2014
+// ---------------------
+
+package com.cleversafe.og.log4j2;
+
+import java.util.Locale;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.lookup.StrLookup;
+import org.joda.time.format.DateTimeFormat;
+
+@Plugin(name = "og", category = "Lookup")
+public class OGDatetimeLookup implements StrLookup
+{
+   private static final String DATETIME = DateTimeFormat.forPattern("yyyy-MM-dd_HH.mm.ss")
+         .withLocale(Locale.US).print(System.currentTimeMillis());
+
+   public OGDatetimeLookup()
+   {}
+
+   @Override
+   public String lookup(final String key)
+   {
+      return DATETIME;
+   }
+
+   @Override
+   public String lookup(final LogEvent event, final String key)
+   {
+      return DATETIME;
+   }
+}
