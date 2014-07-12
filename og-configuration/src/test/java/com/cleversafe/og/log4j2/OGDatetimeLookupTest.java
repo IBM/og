425ac2817f50ffae94e70c4d773e01c168cf2bd0
rveitch <rveitch@cleversafe.com>
2014-07-12 11:34:24 -0500

    Added test for OGDatetimeLookup

---
+++ og-configuration/src/test/java/com/cleversafe/og/log4j2/OGDatetimeLookupTest.java
@@ -0,0 +1,38 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Jul 12, 2014
+// ---------------------
+
+package com.cleversafe.og.log4j2;
+
+import org.apache.logging.log4j.core.lookup.StrLookup;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OGDatetimeLookupTest
+{
+   @Test
+   public void testOGDatetimeLookup()
+   {
+      final StrLookup lookup = new OGDatetimeLookup();
+      final String s1 = lookup.lookup(null);
+      final String s2 = lookup.lookup(null);
+      final String s3 = lookup.lookup(null, null);
+      Assert.assertEquals(s1, s2);
+      Assert.assertEquals(s2, s3);
+   }
+}
