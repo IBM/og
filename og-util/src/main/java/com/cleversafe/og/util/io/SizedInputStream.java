8fa6f4ab31140001460114f75690ccf2690bf0b2
rveitch <rveitch@cleversafe.com>
2014-07-13 23:44:02 -0500

    Added SizedInputStream abstract class for InputStreams with a known size

---
+++ og-util/src/main/java/com/cleversafe/og/util/io/SizedInputStream.java
@@ -0,0 +1,27 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Jul 13, 2014
+// ---------------------
+
+package com.cleversafe.og.util.io;
+
+import java.io.InputStream;
+
+public abstract class SizedInputStream extends InputStream
+{
+   public abstract long getSize();
+}
