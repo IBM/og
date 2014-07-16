a395dbe63e44f3f89501dd2ceef82170268001d0
rveitch <rveitch@cleversafe.com>
2014-07-16 15:06:36 -0500

    Added HostConfigListTypeAdapterFactory

---
+++ og-configuration/src/main/java/com/cleversafe/og/json/type/HostConfigListTypeAdapterFactory.java
@@ -0,0 +1,76 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Jul 16, 2014
+// ---------------------
+
+package com.cleversafe.og.json.type;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cleversafe.og.json.HostConfig;
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+public class HostConfigListTypeAdapterFactory implements TypeAdapterFactory
+{
+   private final TypeToken<List<HostConfig>> matchingType;
+
+   public HostConfigListTypeAdapterFactory()
+   {
+      this.matchingType = new TypeToken<List<HostConfig>>()
+      {};
+   }
+
+   @Override
+   public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> type)
+   {
+      if (!this.matchingType.equals(type))
+         return null;
+
+      final TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
+
+      return new TypeAdapter<T>()
+      {
+         @Override
+         public void write(final JsonWriter out, final T value) throws IOException
+         {
+            delegate.write(out, value);
+         }
+
+         @Override
+         @SuppressWarnings("unchecked")
+         public T read(final JsonReader in) throws IOException
+         {
+            if (JsonToken.STRING == in.peek())
+            {
+               final List<HostConfig> host = new ArrayList<HostConfig>(1);
+               host.add(new HostConfig(in.nextString()));
+               return (T) host;
+            }
+
+            return delegate.read(in);
+         }
+      }.nullSafe();
+   }
+}
---
+++ og-configuration/src/test/java/com/cleversafe/og/json/type/HostConfigListTypeAdapterFactoryTest.java
@@ -0,0 +1,107 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Jul 16, 2014
+// ---------------------
+
+package com.cleversafe.og.json.type;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cleversafe.og.json.HostConfig;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+public class HostConfigListTypeAdapterFactoryTest
+{
+   private static final double ERR = Math.pow(0.1, 6);
+
+   private HostConfigTypeAdapterFactory hostTypeAdapterFactory;
+   private HostConfigListTypeAdapterFactory hostListTypeAdapterFactory;
+   private TypeToken<List<HostConfig>> typeToken;
+   private Gson gson;
+
+   @Before
+   public void before()
+   {
+      this.hostTypeAdapterFactory = new HostConfigTypeAdapterFactory();
+      this.hostListTypeAdapterFactory = new HostConfigListTypeAdapterFactory();
+      this.typeToken = new TypeToken<List<HostConfig>>()
+      {};
+      this.gson = new GsonBuilder()
+            .registerTypeAdapterFactory(this.hostTypeAdapterFactory)
+            .registerTypeAdapterFactory(this.hostListTypeAdapterFactory)
+            .create();
+   }
+
+   @Test
+   public void testNonHostConfigList()
+   {
+      Assert.assertNull(this.hostListTypeAdapterFactory.create(this.gson,
+            TypeToken.get(String.class)));
+   }
+
+   @Test
+   public void testHostConfigList()
+   {
+      Assert.assertNotNull(this.hostListTypeAdapterFactory.create(this.gson, this.typeToken));
+   }
+
+   @Test
+   public void testFullHostConfigList()
+   {
+      final String json = "[{\"host\": \"127.0.0.1\", \"weight\": 3.5}, \"192.168.8.1\"]";
+      final List<HostConfig> config = this.gson.fromJson(json, this.typeToken.getType());
+
+      Assert.assertEquals(2, config.size());
+      final HostConfig h1 = config.get(0);
+      Assert.assertEquals("127.0.0.1", h1.getHost());
+      Assert.assertEquals(3.5, h1.getWeight(), ERR);
+      final HostConfig h2 = config.get(1);
+      Assert.assertEquals("192.168.8.1", h2.getHost());
+      Assert.assertEquals(1.0, h2.getWeight(), ERR);
+   }
+
+   @Test
+   public void testStringHostConfig()
+   {
+      final String json = "10.10.1.1";
+      final List<HostConfig> config = this.gson.fromJson(json, this.typeToken.getType());
+
+      Assert.assertEquals(1, config.size());
+      final HostConfig h1 = config.get(0);
+      Assert.assertEquals("10.10.1.1", h1.getHost());
+      Assert.assertEquals(1.0, h1.getWeight(), ERR);
+   }
+
+   @Test
+   public void testSerialization()
+   {
+      final List<HostConfig> config = new ArrayList<HostConfig>();
+      config.add(new HostConfig("127.0.0.1"));
+
+      final String typeAdapterSerialization = this.gson.toJson(config);
+      final String defaultSerialization = new GsonBuilder().create().toJson(config);
+
+      Assert.assertEquals(defaultSerialization, typeAdapterSerialization);
+   }
+}
