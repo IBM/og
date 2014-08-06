e4443f5b3baed09193fd1796c5e583dbef6c8f0c
rveitch <rveitch@cleversafe.com>
2014-08-06 00:41:28 -0500

    Reworked object-file to support converting to and from object files

--- og-configuration/src/main/java/com/cleversafe/og/cli/ObjectFile.java
+++ og-configuration/src/main/java/com/cleversafe/og/cli/ObjectFile.java
@@ -19,50 +19,99 @@
 
 package com.cleversafe.og.cli;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
+import com.google.common.base.Charsets;
 import com.google.common.io.BaseEncoding;
+import com.google.common.io.ByteStreams;
 import com.martiansoftware.jsap.JSAPResult;
 
 public class ObjectFile extends AbstractCLI
 {
    private static final String JSAP_RESOURCE_NAME = "objectfile.jsap";
-   // TODO place this constant in util somewhere?
+   private static final BaseEncoding ENCODING = BaseEncoding.base16().lowerCase();
    private static int ID_LENGTH = 18;
 
    public static void main(final String[] args)
    {
       final JSAPResult jsapResult = processArgs(JSAP_RESOURCE_NAME, args);
-
       try
       {
-         readIdFile(jsapResult.getFile("object_file"));
+         final InputStream in = getInputStream(jsapResult.getFile("input"));
+         final OutputStream out = getOutputStream(jsapResult.getFile("output"));
+         if (jsapResult.getBoolean("write"))
+            write(in, out);
+         else if (jsapResult.getBoolean("read"))
+            read(in, out);
+         else
+            identity(in, out);
+
+         if (!out.equals(System.out))
+            out.close();
       }
       catch (final IOException e)
       {
-         _consoleLogger.error("Exception reading object file", e);
+         _consoleLogger.error("", e);
          System.exit(UNKNOWN_ERROR);
       }
    }
 
-   public static void readIdFile(final File filename) throws IOException
+   public static InputStream getInputStream(final File input) throws FileNotFoundException
    {
-      FileInputStream in = null;
+      if (input != null)
+         return new FileInputStream(input);
+      return System.in;
+   }
+
+   public static OutputStream getOutputStream(final File output) throws FileNotFoundException
+   {
+      if (output != null)
+         return new FileOutputStream(output);
+      return System.out;
+   }
+
+   public static void write(final InputStream in, final OutputStream out) throws IOException
+   {
+      final BufferedReader reader = new BufferedReader(new InputStreamReader(in, Charsets.UTF_8));
+      String line;
+      while ((line = reader.readLine()) != null)
+      {
+         out.write(ENCODING.decode(line));
+      }
+   }
+
+   public static void read(final InputStream in, final OutputStream out) throws IOException
+   {
+      BufferedWriter writer = null;
       try
       {
-         in = new FileInputStream(filename);
+         writer = new BufferedWriter(new OutputStreamWriter(out, Charsets.UTF_8));
          final byte[] buf = new byte[ID_LENGTH];
          while (in.read(buf) == ID_LENGTH)
          {
-            _consoleLogger.info(BaseEncoding.base16().lowerCase().encode(buf));
+            writer.write(ENCODING.encode(buf));
+            writer.newLine();
          }
       }
       finally
       {
-         if (in != null)
-            in.close();
+         if (writer != null)
+            writer.flush();
       }
+   }
+
+   public static void identity(final InputStream in, final OutputStream out) throws IOException
+   {
+      ByteStreams.copy(in, out);
    }
 }
