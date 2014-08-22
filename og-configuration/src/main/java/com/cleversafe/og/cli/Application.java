ee57c7cc4fb2b2c25d01790edc794a1b0e4acc45
rveitch <rveitch@cleversafe.com>
2014-08-22 16:21:05 -0500

    Added Application class for assisting in the creation of cli classes

---
+++ og-configuration/src/main/java/com/cleversafe/og/cli/Application.java
@@ -0,0 +1,162 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Aug 22, 2014
+// ---------------------
+
+package com.cleversafe.og.cli;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Iterator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cleversafe.og.util.Version;
+import com.google.common.base.Charsets;
+import com.google.gson.Gson;
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPResult;
+
+public class Application
+{
+   public static final int EXIT_CONFIGURATION = 1;
+
+   private Application()
+   {}
+
+   public static Cli cli(final String name, final String jsapResourceName, final String[] args)
+   {
+      return new Cli(name, jsapResourceName, args);
+   }
+
+   public static class Cli
+   {
+      private static final Logger _consoleLogger = LoggerFactory.getLogger("ConsoleLogger");
+      private final String name;
+      private final JSAP jsap;
+      private final JSAPResult jsapResult;
+
+      private Cli(final String name, final String jsapResourceName, final String[] args)
+      {
+         this.name = checkNotNull(name);
+         checkNotNull(jsapResourceName);
+         checkNotNull(args);
+         try
+         {
+            this.jsap = new JSAP(Application.getResource(jsapResourceName).toURL());
+            this.jsapResult = this.jsap.parse(args);
+         }
+         catch (final Exception e)
+         {
+            throw new IllegalArgumentException(e);
+         }
+      }
+
+      public boolean shouldStop()
+      {
+         return error() || help() || version();
+      }
+
+      public boolean error()
+      {
+         return !this.jsapResult.success();
+      }
+
+      public boolean help()
+      {
+         return this.jsapResult.getBoolean("help");
+      }
+
+      public boolean version()
+      {
+         return this.jsapResult.getBoolean("version");
+      }
+
+      public JSAPResult flags()
+      {
+         return this.jsapResult;
+      }
+
+      public void printUsage()
+      {
+         _consoleLogger.info("Usage: {} {}", this.name, this.jsap.getUsage());
+         _consoleLogger.info(this.jsap.getHelp());
+      }
+
+      public void printErrors()
+      {
+         @SuppressWarnings("rawtypes")
+         final Iterator errs = this.jsapResult.getErrorMessageIterator();
+         while (errs.hasNext())
+         {
+            _consoleLogger.error("{}", errs.next());
+         }
+      }
+
+      public void printVersion()
+      {
+         _consoleLogger.info(Version.displayVersion());
+      }
+   }
+
+   public static URI getResource(final String resourceName)
+   {
+      checkNotNull(resourceName);
+      try
+      {
+         final URL url = ClassLoader.getSystemResource(resourceName);
+
+         if (url == null)
+            throw new IllegalArgumentException(String.format(
+                  "Could not find configuration file on classpath [%s]", resourceName));
+
+         return url.toURI();
+      }
+      catch (final URISyntaxException e)
+      {
+         throw new IllegalArgumentException(e);
+      }
+   }
+
+   public static <T> T fromJson(
+         final File userJson,
+         final File defaultJson,
+         final Class<T> cls,
+         final Gson gson) throws FileNotFoundException
+   {
+      final File json = userJson != null ? userJson : checkNotNull(defaultJson);
+      checkNotNull(cls);
+      checkNotNull(gson);
+
+      final Reader reader = new InputStreamReader(new FileInputStream(json), Charsets.UTF_8);
+      return gson.fromJson(reader, cls);
+   }
+
+   public static void exit(final int exitCode)
+   {
+      System.exit(exitCode);
+   }
+}
---
+++ og-configuration/src/test/java/com/cleversafe/og/cli/ApplicationTest.java
@@ -0,0 +1,189 @@
+//
+// Copyright (C) 2005-2011 Cleversafe, Inc. All rights reserved.
+//
+// Contact Information:
+// Cleversafe, Inc.
+// 222 South Riverside Plaza
+// Suite 1700
+// Chicago, IL 60606, USA
+//
+// licensing@cleversafe.com
+//
+// END-OF-HEADER
+//
+// -----------------------
+// @author: rveitch
+//
+// Date: Aug 22, 2014
+// ---------------------
+
+package com.cleversafe.og.cli;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+import com.cleversafe.og.cli.Application.Cli;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+@RunWith(DataProviderRunner.class)
+public class ApplicationTest
+{
+   @Rule
+   public ExpectedException thrown = ExpectedException.none();
+   private static final String APPLICATION_JSAP = "application.jsap";
+   private static final String APPLICATION_JSON = "application.json";
+   private static final String DEFAULT_APPLICATION_JSON = "defaultApplication.json";
+   private static final String VALUE = "value";
+   private static final String DEFAULT_VALUE = "defaultValue";
+   private File defaultJson;
+   private Gson gson;
+
+   @Before
+   public void before()
+   {
+      this.defaultJson = new File(Application.getResource(DEFAULT_APPLICATION_JSON));
+      this.gson = new GsonBuilder().create();
+   }
+
+   @DataProvider
+   public static Object[][] provideInvalidCli()
+   {
+      final String app = "application";
+      final String jsap = APPLICATION_JSAP;
+      final String[] args = new String[]{};
+
+      return new Object[][]{
+            {null, jsap, args, NullPointerException.class},
+            {app, null, args, NullPointerException.class},
+            {app, jsap, null, NullPointerException.class},
+            {app, "nonexistent.jsap", args, IllegalArgumentException.class}
+      };
+   }
+
+   @Test
+   @UseDataProvider("provideInvalidCli")
+   public void invalidCli(
+         final String name,
+         final String jsapResourceName,
+         final String[] args,
+         final Class<Exception> expectedException)
+   {
+      this.thrown.expect(expectedException);
+      Application.cli(name, jsapResourceName, args);
+   }
+
+   @DataProvider
+   public static Object[][] provideCli()
+   {
+      return new Object[][]{
+            // args, shouldStop, error, help, version
+            {new String[]{}, false, false, false, false},
+            {new String[]{"--nonexistent"}, true, true, false, false},
+            {new String[]{"-h"}, true, false, true, false},
+            {new String[]{"-v"}, true, false, false, true}
+      };
+   }
+
+   @Test
+   @UseDataProvider("provideCli")
+   public void cli(
+         final String[] args,
+         final boolean shouldStop,
+         final boolean error,
+         final boolean help,
+         final boolean version)
+   {
+      final Cli cli = Application.cli("application", APPLICATION_JSAP, args);
+      assertThat(cli.shouldStop(), is(shouldStop));
+      assertThat(cli.error(), is(error));
+      assertThat(cli.help(), is(help));
+      assertThat(cli.version(), is(version));
+      assertThat(cli.flags(), notNullValue());
+
+      // not a good way to validate these so just call them and make sure they don't throw
+      cli.printUsage();
+      cli.printErrors();
+      cli.printVersion();
+   }
+
+   @Test(expected = NullPointerException.class)
+   public void getResourceNullResource()
+   {
+      Application.getResource(null);
+   }
+
+   @Test(expected = IllegalArgumentException.class)
+   public void getResourceMissingResource()
+   {
+      Application.getResource("nonexistent.jsap");
+   }
+
+   @Test
+   public void getResource()
+   {
+      assertThat(Application.getResource(APPLICATION_JSAP), notNullValue());
+   }
+
+   @DataProvider
+   public static Object[][] provideInvalidFromJson()
+   {
+      final File defaultJson = new File(Application.getResource(DEFAULT_APPLICATION_JSON));
+      final File nonExistent = new File("/nonexistent");
+      final Gson gson = new GsonBuilder().create();
+
+      return new Object[][]{
+            {null, null, Item.class, gson, NullPointerException.class},
+            {null, defaultJson, null, gson, NullPointerException.class},
+            {null, defaultJson, Item.class, null, NullPointerException.class},
+            {null, nonExistent, Item.class, gson, FileNotFoundException.class},
+            {nonExistent, defaultJson, Item.class, gson, FileNotFoundException.class}
+      };
+   }
+
+   static class Item
+   {
+      public String key;
+   }
+
+   @Test
+   @UseDataProvider("provideInvalidFromJson")
+   public void invalidFromJson(
+         final File userJson,
+         final File defaultJson,
+         final Class<?> cls,
+         final Gson gson,
+         final Class<Exception> expectedException) throws FileNotFoundException
+   {
+      this.thrown.expect(expectedException);
+      Application.fromJson(userJson, defaultJson, cls, gson);
+   }
+
+   @Test
+   public void fromJsonDefault() throws FileNotFoundException
+   {
+      final Item item = Application.fromJson(null, this.defaultJson, Item.class, this.gson);
+      assertThat(item.key, is(DEFAULT_VALUE));
+   }
+
+   @Test
+   public void fromJson() throws FileNotFoundException
+   {
+      final File json = new File(Application.getResource(APPLICATION_JSON));
+      final Item item = Application.fromJson(json, this.defaultJson, Item.class, this.gson);
+      assertThat(item.key, is(VALUE));
+   }
+}
