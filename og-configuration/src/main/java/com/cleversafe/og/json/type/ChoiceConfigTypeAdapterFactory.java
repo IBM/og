051c9ffd4074f4558abdf5a3946f05091acb43b3
rveitch <rveitch@cleversafe.com>
2015-04-21 23:45:49 -0500

    Modify SelectionConfig and ChoiceConfig parsing to be safer; throw exceptions earlier during json parsing rather than at test run time

--- og-configuration/src/main/java/com/cleversafe/og/json/type/ChoiceConfigTypeAdapterFactory.java
+++ og-configuration/src/main/java/com/cleversafe/og/json/type/ChoiceConfigTypeAdapterFactory.java
@@ -9,16 +9,27 @@
 package com.cleversafe.og.json.type;
 
 import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Map;
 
 import com.cleversafe.og.json.ChoiceConfig;
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.TypeAdapterFactory;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 public class ChoiceConfigTypeAdapterFactory implements TypeAdapterFactory {
+  private static final Map<Type, JsonToken> TOKENS = ImmutableMap.<Type, JsonToken>builder()
+      .put(Byte.class, JsonToken.NUMBER).put(Double.class, JsonToken.NUMBER)
+      .put(Float.class, JsonToken.NUMBER).put(Integer.class, JsonToken.NUMBER)
+      .put(Long.class, JsonToken.NUMBER).put(Short.class, JsonToken.NUMBER)
+      .put(Boolean.class, JsonToken.BOOLEAN).put(String.class, JsonToken.STRING).build();
+
   @Override
   @SuppressWarnings("unchecked")
   public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> type) {
@@ -27,6 +38,7 @@ public class ChoiceConfigTypeAdapterFactory implements TypeAdapterFactory {
       return null;
 
     final TypeAdapter<T> delegate = gson.getDelegateAdapter(this, type);
+    final Type genericType = ((ParameterizedType) type.getType()).getActualTypeArguments()[0];
 
     return new TypeAdapter<T>() {
       @Override
@@ -36,16 +48,19 @@ public class ChoiceConfigTypeAdapterFactory implements TypeAdapterFactory {
 
       @Override
       public T read(final JsonReader in) throws IOException {
-        switch (in.peek()) {
-          case BOOLEAN:
-            return (T) new ChoiceConfig<Boolean>(in.nextBoolean());
-          case NUMBER:
-            return (T) new ChoiceConfig<Double>(in.nextDouble());
-          case STRING:
-            return (T) new ChoiceConfig<String>(in.nextString());
-          default:
-            return delegate.read(in);
+        JsonToken genericToken = ChoiceConfigTypeAdapterFactory.TOKENS.get(genericType);
+
+        if (genericToken != null && genericToken.equals(in.peek())) {
+          switch (in.peek()) {
+            case BOOLEAN:
+              return (T) new ChoiceConfig<Boolean>(in.nextBoolean());
+            case NUMBER:
+              return (T) new ChoiceConfig<Double>(in.nextDouble());
+            default:
+              return (T) new ChoiceConfig<String>(in.nextString());
+          }
         }
+        return delegate.read(in);
       }
     }.nullSafe();
   }
--- og-configuration/src/main/java/com/cleversafe/og/json/type/SelectionConfigTypeAdapterFactory.java
+++ og-configuration/src/main/java/com/cleversafe/og/json/type/SelectionConfigTypeAdapterFactory.java
@@ -9,10 +9,12 @@
 package com.cleversafe.og.json.type;
 
 import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 import com.cleversafe.og.json.ChoiceConfig;
 import com.cleversafe.og.json.SelectionConfig;
+import com.google.common.reflect.TypeParameter;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.TypeAdapterFactory;
@@ -21,6 +23,7 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
 public class SelectionConfigTypeAdapterFactory implements TypeAdapterFactory {
+
   @Override
   @SuppressWarnings("unchecked")
   public <T> TypeAdapter<T> create(final Gson gson, final TypeToken<T> type) {
@@ -37,23 +40,55 @@ public class SelectionConfigTypeAdapterFactory implements TypeAdapterFactory {
       }
 
       @Override
-      @SuppressWarnings("rawtypes")
       public T read(final JsonReader in) throws IOException {
+        final Class<?> genericType =
+            (Class<?>) ((ParameterizedType) type.getType()).getActualTypeArguments()[0];
+
         switch (in.peek()) {
           case BOOLEAN:
-            return (T) new SelectionConfig<Boolean>(in.nextBoolean());
           case NUMBER:
-            return (T) new SelectionConfig<Double>(in.nextDouble());
           case STRING:
-            return (T) new SelectionConfig<String>(in.nextString());
-          case BEGIN_ARRAY: {
-            SelectionConfig config = new SelectionConfig();
-            config.choices = gson.getAdapter(new TypeToken<List<ChoiceConfig>>() {}).read(in);
-            return (T) config;
-          }
+            return (T) primitive(genericType, in);
+          case BEGIN_ARRAY:
+            return (T) list(genericType, in);
           default:
             return delegate.read(in);
         }
+      }
+
+      private <S> SelectionConfig<S> primitive(Class<S> clazz, JsonReader in) throws IOException {
+        SelectionConfig<S> config = new SelectionConfig<S>();
+        config.choices.add(gson.getAdapter(primitiveToken(clazz)).read(in));
+        return config;
+      }
+
+      private <S> SelectionConfig<S> list(Class<S> clazz, JsonReader in) throws IOException {
+        SelectionConfig<S> config = new SelectionConfig<S>();
+        config.choices = gson.getAdapter(listToken(clazz)).read(in);
+        return config;
+      }
+
+      /*
+       * must use guava's TypeToken implementation to create a TypeToken instance with a dynamic
+       * type; then convert back to gson's TypeToken implementation for use in calling code. See:
+       * https://groups.google.com/forum/#!topic/guava-discuss/HdBuiO44uaw
+       */
+      private <S> TypeToken<ChoiceConfig<S>> primitiveToken(Class<S> clazz) {
+        @SuppressWarnings("serial")
+        com.google.common.reflect.TypeToken<ChoiceConfig<S>> choiceToken =
+            new com.google.common.reflect.TypeToken<ChoiceConfig<S>>() {}.where(
+                new TypeParameter<S>() {}, com.google.common.reflect.TypeToken.of(clazz));
+
+        return (TypeToken<ChoiceConfig<S>>) TypeToken.get(choiceToken.getType());
+      }
+
+      private <S> TypeToken<List<ChoiceConfig<S>>> listToken(Class<S> clazz) {
+        @SuppressWarnings("serial")
+        com.google.common.reflect.TypeToken<List<ChoiceConfig<S>>> choiceToken =
+            new com.google.common.reflect.TypeToken<List<ChoiceConfig<S>>>() {}.where(
+                new TypeParameter<S>() {}, com.google.common.reflect.TypeToken.of(clazz));
+
+        return (TypeToken<List<ChoiceConfig<S>>>) TypeToken.get(choiceToken.getType());
       }
     }.nullSafe();
   }
