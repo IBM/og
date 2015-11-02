bae9aefb6fae018d24377d40ee233a4e04a6b17a
Robert Veitch <rveitch@cleversafe.com>
2015-11-01 20:47:19 -0600

    OG-58 Enhancements to console summary

--- og-configuration/src/main/java/com/cleversafe/og/cli/Summary.java
+++ og-configuration/src/main/java/com/cleversafe/og/cli/Summary.java
@@ -11,6 +11,7 @@ package com.cleversafe.og.cli;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -22,6 +23,9 @@ import org.joda.time.format.DateTimeFormatter;
 import com.cleversafe.og.statistic.Counter;
 import com.cleversafe.og.statistic.Statistics;
 import com.cleversafe.og.util.Operation;
+import com.cleversafe.og.util.Pair;
+import com.cleversafe.og.util.SizeUnit;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSortedMap;
 
 /**
@@ -79,37 +83,77 @@ public class Summary {
           FORMATTER.print(this.timestampFinish), this.runtime, this.operations, this.write,
           this.read, this.delete);
     }
-  }
 
-  static class OperationStats {
-    final transient Operation operation;
-    final long operations;
-    final long bytes;
-    final Map<Integer, Long> statusCodes;
+    class OperationStats {
+      final transient Operation operation;
+      final long operations;
+      final long bytes;
+      final Map<Integer, Long> statusCodes;
 
-    private OperationStats(final Statistics stats, final Operation operation) {
-      this.operation = operation;
-      this.operations = stats.get(operation, Counter.OPERATIONS);
-      this.bytes = stats.get(operation, Counter.BYTES);
-      this.statusCodes = ImmutableSortedMap.copyOf(stats.statusCodes(operation));
-    }
-
-    @Override
-    public String toString() {
-      return String.format("[%s]%nOperations: %s%nBytes: %s%nStatus Codes:%n%s%n", this.operation,
-          this.operations, this.bytes, formatStatusCodes());
-    }
-
-    private String formatStatusCodes() {
-      if (this.statusCodes.isEmpty()) {
-        return String.format("N/A%n");
+      private OperationStats(final Statistics stats, final Operation operation) {
+        this.operation = operation;
+        this.operations = stats.get(operation, Counter.OPERATIONS);
+        this.bytes = stats.get(operation, Counter.BYTES);
+        this.statusCodes = ImmutableSortedMap.copyOf(stats.statusCodes(operation));
       }
 
-      final StringBuilder s = new StringBuilder();
-      for (final Entry<Integer, Long> sc : this.statusCodes.entrySet()) {
-        s.append(String.format("%s: %s%n", sc.getKey(), sc.getValue()));
+      @Override
+      public String toString() {
+        return String.format(
+            "[%s]%n" + "Operations: %s%n" + "%s%n" + "%s%n" + "%s%n" + "Status Codes:%n%s%n",
+            this.operation, this.operations, formatBytes(), formatThroughput(), formatOPS(),
+            formatStatusCodes());
       }
-      return s.toString();
+
+      // determine whether to display byte total in gb, mb, kb or bytes
+      private String formatBytes() {
+        final Pair<Double, SizeUnit> displaySize = displaySize(this.bytes);
+        String displayUnit = displaySize.getValue().toString().toLowerCase();
+        // capitalize
+        displayUnit = displayUnit.substring(0, 1).toUpperCase() + displayUnit.substring(1);
+        return String.format("%s: %.2f", displayUnit, displaySize.getKey());
+      }
+
+      private String formatThroughput() {
+        final double bytesPerSecond = this.bytes / SummaryStats.this.runtime;
+        final Pair<Double, SizeUnit> displaySize = displaySize(bytesPerSecond);
+        String displayUnit;
+        if (displaySize.getValue() != SizeUnit.BYTES) {
+          displayUnit = String.format("%sB/s", displaySize.getValue().toString().substring(0, 1));
+        } else {
+          displayUnit = "B/s";
+        }
+        return String.format("Throughput: %.2f %s", displaySize.getKey(), displayUnit);
+      }
+
+      private String formatOPS() {
+        final double operationsPerSecond = this.operations / SummaryStats.this.runtime;
+        return String.format("OPS: %.2f", operationsPerSecond);
+      }
+
+      private String formatStatusCodes() {
+        if (this.statusCodes.isEmpty()) {
+          return String.format("N/A%n");
+        }
+
+        final StringBuilder s = new StringBuilder();
+        for (final Entry<Integer, Long> sc : this.statusCodes.entrySet()) {
+          s.append(String.format("%s: %s%n", sc.getKey(), sc.getValue()));
+        }
+        return s.toString();
+      }
+
+      private Pair<Double, SizeUnit> displaySize(final double bytes) {
+        final List<SizeUnit> units = ImmutableList.of(SizeUnit.TERABYTES, SizeUnit.GIGABYTES,
+            SizeUnit.MEGABYTES, SizeUnit.KILOBYTES);
+        for (final SizeUnit unit : units) {
+          final double size = bytes / unit.toBytes(1);
+          if (size >= 1.0) {
+            return Pair.of(size, unit);
+          }
+        }
+        return Pair.of(bytes, SizeUnit.BYTES);
+      }
     }
   }
 
