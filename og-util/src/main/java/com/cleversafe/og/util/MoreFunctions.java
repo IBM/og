33b488d05e60004374b5ee2270a2fcad38876872
Robert Veitch <rveitch@cleversafe.com>
2016-02-18 17:07:03 -0600

    Add lookupKey Function; add forSupplier toString

--- og-util/src/main/java/com/cleversafe/og/util/MoreFunctions.java
+++ og-util/src/main/java/com/cleversafe/og/util/MoreFunctions.java
@@ -10,6 +10,8 @@ package com.cleversafe.og.util;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.Map;
+
 import javax.annotation.Nullable;
 
 import com.google.common.base.Function;
@@ -45,5 +47,31 @@ public class MoreFunctions {
     public T apply(@Nullable final O input) {
       return this.supplier.get();
     }
+
+    @Override
+    public String toString() {
+      return String.format("SupplierFunction [%s]", this.supplier);
+    }
+  }
+
+  /**
+   * Creates a function which returns its value by looking up a key in an input map
+   * 
+   * @param key the lookup key
+   * @return the value stored for the key in the input map
+   * @throws NullPointerException if value is null
+   */
+  public static Function<Map<String, String>, String> keyLookup(final String key) {
+    return new Function<Map<String, String>, String>() {
+      @Override
+      public String apply(final Map<String, String> input) {
+        return checkNotNull(input.get(key));
+      }
+
+      @Override
+      public String toString() {
+        return String.format("KeyLookupFunction [%s]", key);
+      }
+    };
   }
 }
