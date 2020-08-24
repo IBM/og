d74a6afbcbaa7044ff3f1172535156e0df62f868
Nazir Ahmed <nazir.ahmed@ibm.com>
2020-08-24 10:33:39 -0500

    Add license header

--- og-supplier/src/main/java/com/ibm/og/supplier/CredentialGetterFunction.java
+++ og-supplier/src/main/java/com/ibm/og/supplier/CredentialGetterFunction.java
@@ -1,3 +1,8 @@
+/* Copyright (c) IBM Corporation 2020. All Rights Reserved.
+ * Project name: Object Generator
+ * This project is licensed under the Apache License 2.0, see LICENSE.
+ */
+
 package com.ibm.og.supplier;
 
 import com.google.common.collect.ImmutableList;
--- og-supplier/src/test/java/com/ibm/og/supplier/CredentialGetterFunctionTest.java
+++ og-supplier/src/test/java/com/ibm/og/supplier/CredentialGetterFunctionTest.java
@@ -1,3 +1,8 @@
+/* Copyright (c) IBM Corporation 2020. All Rights Reserved.
+ * Project name: Object Generator
+ * This project is licensed under the Apache License 2.0, see LICENSE.
+ */
+
 package com.ibm.og.supplier;
 
 import com.google.gson.FieldNamingPolicy;
@@ -52,7 +57,7 @@ public class CredentialGetterFunctionTest {
     }
   }
 
-  
+
   @Test
   public void credentialGetterFunctionCompactTest() throws  Exception{
 
